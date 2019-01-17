be2b48ab93388e6d1fefedaa5b4742c773dd1885
Anand Kumar Mainali <urajmainali5@gmail.com>
2019-01-17 13:44:19 +0545

    Prepared for deployed

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const passport = require("passport");
 const users = require("./routes/api/users");
 const posts = require("./routes/api/posts");
 const profile = require("./routes/api/profile");
+const path = require("path");
 
 const app = express();
 
@@ -35,6 +36,16 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/posts", posts);
 app.use("/api/profile", profile);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
